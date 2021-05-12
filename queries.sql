3cf499dc8c4ecfa5b46eab4f89b41a3792100a80
Casey Juergens <79878212+caseyjuergens@users.noreply.github.com>
2021-05-12 11:18:05 -0700

    question 5 complete

--- queries.sql
+++ queries.sql
@@ -32,7 +32,9 @@ INNER JOIN departments d ON
 de.dept_no = d.dept_no
 -------------------------------------------------------
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
+SELECT e.first_name, e.last_name, e.sex
+FROM employees e
+WHERE e.first_name = 'Hercules' AND e.last_name LIKE 'B%'
 
 
 --------------------------------------------------------
